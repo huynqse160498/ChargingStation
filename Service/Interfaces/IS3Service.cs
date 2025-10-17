b303e32e08550f860a3a8f7efb49449ff293a62f
QuangHuy <huynqse160498@fpt.edu.vn>
2025-10-17 14:17:08 +0700

    create s3service

---
+++ Service/Interfaces/IS3Service.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Services.Interfaces
+{
+    public interface IS3Service
+    {
+        Task<string> UploadFileAsync(IFormFile file, string folder);
+        Task<bool> DeleteFileAsync(string fileUrl);
+        Task<string> RenameFileAsync(string oldFileUrl, string newFileName);
+    }
+}
