7d419f9d13bf7c1e10403b35d1087b45541fd4b2
thiennhat-ctrl <tkzoro5@gmail.com>
2025-10-19 17:05:45 +0700

    CRUD SubscriptionPlan

---
+++ Repository/Interfaces/ISubscriptionPlanRepository.cs
@@ -0,0 +1,19 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interfaces
+{
+    public interface ISubscriptionPlanRepository
+    {
+        Task<List<SubscriptionPlan>> GetAllAsync();                                   
+        Task<SubscriptionPlan?> GetByIdAsync(int id);                                 
+        Task<bool> ExistsNameAsync(string planName, int? ignoreId = null);            
+        Task<SubscriptionPlan> AddAsync(SubscriptionPlan entity);                     
+        Task<SubscriptionPlan> UpdateAsync(SubscriptionPlan entity);                 
+        Task DeleteAsync(SubscriptionPlan entity);
+    }
+}
---
+++ Service/Interfaces/ISubscriptionPlanService.cs
@@ -0,0 +1,19 @@
+﻿using Repositories.DTOs.SubscriptionPlans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Services.Interfaces
+{
+    public interface ISubscriptionPlanService
+    {
+        Task<IEnumerable<SubscriptionPlanReadDto>> GetAllAsync();                 
+        Task<SubscriptionPlanReadDto> GetByIdAsync(int id);                       
+        Task<SubscriptionPlanReadDto> CreateAsync(SubscriptionPlanCreateDto dto);
+        Task<SubscriptionPlanReadDto> UpdateAsync(int id, SubscriptionPlanUpdateDto dto);
+        Task DeleteAsync(int id);                                                 
+    }
+}
